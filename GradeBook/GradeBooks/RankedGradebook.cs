ffd6ae85ef49a4e6e38634b8fa9db7e55078b21a
Mayur Meshram <mmeshram43@gmail.com>
2022-02-21 09:58:37 +0530

    added

---
+++ GradeBook/GradeBooks/RankedGradebook.cs
@@ -0,0 +1,15 @@
+using GradeBook.Enums ;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+         public RankedGradeBook(string name) : base(name)
+        {
+          Type = GradeBookType.Ranked ;
+        }
+
+        
+
+    }
+}
